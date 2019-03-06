ee8eef9df2978b73235fa74f662d6c613ec80fc8
Serge Vei <sergevei59@gmail.com>
2019-03-06 18:26:20 +0300

    start developing on new branch

---
+++ routers/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   api/users/test
+// @desc    Test page
+// @access  Public 
+router.get("/test" , (req , res)=>res.json({msg:"test users"}));
+
+module.exports = router;
