7c515bcd71cc4b7fa33b955e7d6e5cc2d10accd2
sergevei <sergevei59@gmail.com>
2019-03-06 15:17:46 +0300

    upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/",(rec , res )=>{
+    res.send("Hello SERVER!!!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port , ()=>{
+    console.log(`Server running on port ${port}`);
+});
