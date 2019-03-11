b375a2a5c61a8fe25a14f54d119fb9fff2b85689
Serge Vei <sergevei59@gmail.com>
2019-03-11 18:28:57 +0300

    working version :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import './App.css';
+
 import Header from './components/header';
 import Login from './components/login';
 import Register from './components/register';
@@ -6,36 +8,36 @@ import LeftSideBar from './components/left_sidebar';
 import MainContent from './components/main_content';
 import Latest from './components/right_sidebar/latest';
 import Popular from './components/right_sidebar/popular';
+import 'bootstrap-4-grid';
 
-export default  class App extends Component {
-    render(){
-        return(
-            <div className="row">
-               <div className="col-md-12">
-                  <Header/>
-               </div>
-
-               <div className="col-md-3">
-                  <p>Category</p>
-                  <LeftSideBar/>
-               </div>
-               <div className="col-md-6">
-                  <MainContent/>
-               </div>
-               <div className="col-md-3">
-                  <p>Latest</p>
-                  <Latest/>
-                  <p>Popular</p>
-                  <Popular/>
-               </div>
-                
-               <div className="col-md-6">
-                  <Login/>
-               </div>
-               <div className="col-md-6">
-                  <Register/>
-               </div>
-            </div>
-        );
-    }
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <Header/>
+        <div className="col-md-3">
+            <p>Category</p>
+            <LeftSideBar/>
+        </div>
+        <div className="col-md-6">
+            <MainContent/>
+        </div>
+        <div className="col-md-3">
+            <p>Latest</p>
+            <Latest/>
+            <p>Popular</p>
+            <Popular/>
+        </div>
+          
+        <div className="col-md-6">
+            <Login/>
+        </div>
+        <div className="col-md-6">
+            <Register/>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
